70cc33ac8c61800bfba380078cc8d963d745b08a
YeongHoon Song <thddudgns972@gmail.com>
2023-10-30 17:28:12 +0900

    Update ARView
    ARSceneView
    - Mesh추가 매소드화 해서 Coordinator로 이동
    - To Do로 차후 변경점 주석으로 정리
    Coordinator
    - 텍스트 매쉬 커스텀
    - 탭 제스처 인식시 커스텀 매쉬 ARView에 추가
    - To Do로 차후 변경점 주석으로 정리

--- ButtonARGuide/ARView/ARSceneView.swift
+++ ButtonARGuide/ARView/ARSceneView.swift
@@ -11,62 +11,47 @@ import SwiftUI
 import ARKit
 import RealityKit
 
-
-struct ARSceneView : View {
-    var body: some View {
-        ARViewContainer().edgesIgnoringSafeArea(.all)
-    }
-}
-
 struct ARViewContainer: UIViewRepresentable {
-    // var model
+    // To Do: 모델 추가
     
     // MARK: - Create UIView(UIKit) & initialize
     func makeUIView(context: Context) -> ARView {
         print("AR DUBUG: Make UIView")
+        // To Do: Navigation bar에 사용법 버튼 생성 및 사용법 페이지 생성
         
         // Screen size : 1179*2556(iPhone 15 pro)
         let arView = ARView(frame: .zero)
         print("AR DEBUG: ARView appear")
         
-        // Connect ARView with UIKit
-        // Help control event & networking
-        context.coordinator.view = arView
-        arView.session.delegate = context.coordinator
-        
-        // ARReferenceImage를 이용하여 이미지 트래킹을 하는 방법도 고려
-        // environmentTexturing를 이용해서 텍스처링하는 방법도 고려
+        // To Do: ARReferenceImage를 이용하여 이미지 트래킹을 하는 방법도 고려
+        // To Do: environmentTexturing를 이용해서 텍스처링하는 방법도 고려
         let configuration = ARWorldTrackingConfiguration()
         configuration.planeDetection = [.horizontal, .vertical]
         arView.session.run(configuration)
         
-        
-        let anchor = AnchorEntity(plane: .any)
-        // 일회성인건가...?
-        // 표면에 대해 수평 수직 3D 설정
-
-        
-        // 기본 생성 AR text entity
-         let text = ModelEntity(mesh: MeshResource.generateText("Hello AR", extrusionDepth: 0.01, font: .systemFont(ofSize: 0.03), containerFrame: .zero, alignment: .center, lineBreakMode: .byCharWrapping), materials: [SimpleMaterial(color: .blue, isMetallic: false)])
-        
-         anchor.addChild(text)
-         text.generateCollisionShapes(recursive: true)
-         arView.scene.anchors.append(anchor)
+        // Add tag mesh when (tap) recognized
+        arView.addGestureRecognizer(UITapGestureRecognizer(target: context.coordinator, action: #selector(Coordinator.isViewTapped)))
         
         return arView
     }
     
     func makeCoordinator() -> Coordinator {
-        Coordinator()
+        Coordinator(self)
     }
     
     // MARK: - Update UIView(UIKit)
-    // when SwiftUI interface state change
+    // When SwiftUI interface state change
     func updateUIView(_ uiView: ARView, context: Context) {
         print("AR DEBUG: Update UIView")
     }
 }
 
+// MARK: - Content View
+struct ARSceneView : View {
+    var body: some View {
+        ARViewContainer().edgesIgnoringSafeArea(.all)
+    }
+}
 
 #Preview {
     ARSceneView()
--- ButtonARGuide/ARView/Coordinator.swift
+++ ButtonARGuide/ARView/Coordinator.swift
@@ -10,20 +10,59 @@ import Foundation
 import ARKit
 import RealityKit
 
-class Coordinator: NSObject, ARSessionDelegate {
-
-    weak var view: ARView?
+class Coordinator: NSObject {
+    var arContainer: ARViewContainer
     
-    @objc func handleTap(_ recognizer: UITapGestureRecognizer) {
+    init(_ container: ARViewContainer) {
+        self.arContainer = container
+    }
+    
+    // MARK: - Add textbox when (tap) action
+    // 나중에 이미지 인식시 추가될 수 있도록 변경
+    @objc func isViewTapped(gesture: UITapGestureRecognizer) {
+        guard let arView = gesture.view as? ARView
+        else { return }
         
-        guard let view = self.view else { return }
+        // get tap location
+        let tapPosition = gesture.location(in: arView)
         
-        let tapLocation = recognizer.location(in: view)
-        
-        view.raycast(from: tapLocation, allowing: .estimatedPlane, alignment: .any)
-        if let entity = view.entity(at: tapLocation) as? ModelEntity {
-            let material = SimpleMaterial(color: UIColor.random(), isMetallic: true)
-            entity.model?.materials = [material]
+        // Convert tap position to AR world
+        if let raycastQuery = arView.makeRaycastQuery(
+            from: tapPosition, allowing: .estimatedPlane,
+            alignment: .any),
+        let raycastResult = arView.session.raycast(raycastQuery).first {
+            let anchor = AnchorEntity(world: raycastResult.worldTransform)
+            arView.scene.addAnchor(anchor)
+            
+            // Mesh
+            // To Do: Text string에 model에서 얻은 label 받기
+            let textMesh = MeshResource.generateText(
+                "Button name",
+                extrusionDepth: 0.01,
+                font: .systemFont(ofSize: 0.02),
+                containerFrame: .zero,
+                alignment: .center,
+                lineBreakMode: .byTruncatingMiddle
+            )
+            // To Do: Background 처리 방식 고민해보자
+            let backgroundMesh = MeshResource.generateBox(size: 0.05)
+            
+            // Mesh to Entity
+            let textEntiy = ModelEntity(
+                mesh: textMesh,
+                materials: [SimpleMaterial(color: .black, isMetallic: false)]
+            )
+            let backgroundEntity = ModelEntity(
+                mesh: backgroundMesh,
+                materials: [SimpleMaterial(color: .white, isMetallic: false)]
+            )
+            
+            // Add Entity to ARView
+            anchor.addChild(textEntiy)
+            anchor.addChild(backgroundEntity)
+            arView.scene.addAnchor(anchor)
         }
+        
+        print("AR DEBUG: Tag mesh add")
     }
 }
